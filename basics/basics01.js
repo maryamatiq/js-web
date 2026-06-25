aa1063d4869f99b36f4b87c1e9f95c0592a3602f
maryamatiq <maryamatiq768@gmail.com>
2026-06-25 17:24:51 +0000

    storing basics

---
+++ basics/basics01.js
@@ -0,0 +1,18 @@
+const accId=123
+let accEmail="maryamatiq@gmail.com"
+let accState;
+var accPass="maryam"
+/*
+it is better to 
+not use var
+it is ur choice to use ; or not
+*/
+accCity="isb"
+//accId=246
+accPass="Eman"
+accEmail="eman@gmail.com"
+accCity="pindi"
+
+
+console.log(accCity);
+console.table([accId,accPass,accCity,accEmail,accState]);
