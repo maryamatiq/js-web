71c7128958c0ba331cdabda0c1021fb24868994c
maryamatiq <maryamatiq768@gmail.com>
2026-06-27 11:41:16 +0000

    operations

--- basics/conversion.js
+++ basics/conversion.js
@@ -20,5 +20,40 @@ let boolVal=Boolean(val)
 
 let anynum=33
 let stringnum=String(anynum)
-console.log(typeof stringnum);
-console.log(stringnum)
+// console.log(typeof stringnum);
+// console.log(stringnum)
+
+let value=3;
+let negval=-value;
+//console.log(negval);
+/*
+console.log(2+2)//add
+console.log(2-2)//sub
+console.log(2*2)//product
+console.log(2/2)//division
+console.log(2%2)//modulus
+console.log(2**2)//to take power
+*/
+
+let str1="hello"
+let str2=" world"
+let str3=str1+str2
+//console.log(str3);
+
+console.log(1+"2");//converted to string
+console.log("1"+2);//converted to string
+console.log("1"+2+2);//if 1st val is string then all converted to string
+console.log(1+2+"2");
+//NOT A GOOD PRACTICE SO USE BRACESorParenthesis
+
+console.log(3+4*6/8);//not good
+//the readablity should be high
+
+console.log(+true);//tricky conversion
+
+let x=1
+let y=x++
+let z=++x
+console.log(y);
+console.log(z);
+
