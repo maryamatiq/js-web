d1cb8f4c655b10e9c8a0601ea205242b9c7e7928
maryamatiq <maryamatiq768@gmail.com>
2026-06-27 10:59:12 +0000

    conversion of types

---
+++ basics/conversion.js
@@ -0,0 +1,24 @@
+let age=false
+let con=Number(age)
+//console.log(typeof age)//type-> string
+//console.log(typeof con)//type-> number
+//console.log(con)//Not a Number  
+
+// "33" => 33
+// "33abc"=> NaN
+// null=> 0; undefined=> NaN
+// true=> 1; false=> 0
+
+let val="maryam"
+let boolVal=Boolean(val)
+//console.log(typeof val)
+//console.log(typeof boolVal)
+//console.log(boolVal)
+
+// 1=>true ; 0=> false
+// ""=> false; "maryam"=>true
+
+let anynum=33
+let stringnum=String(anynum)
+console.log(typeof stringnum);
+console.log(stringnum)
