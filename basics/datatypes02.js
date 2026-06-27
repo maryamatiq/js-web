04815d0a82ca54a7bf059138564529c8760774b0
maryamatiq <maryamatiq768@gmail.com>
2026-06-27 10:05:12 +0000

    datatype

---
+++ basics/datatypes02.js
@@ -0,0 +1,21 @@
+"use strict";//to treat all code as a new JS version
+/*alert(3+3)-> we r using node js not browser,
+it is better to not use
+it is used to display pop up msgs */
+/*console.log(3+
+    3)it will run but not readable
+   tc39->ECMA specify standards & mdn
+*/
+
+//PREMITIVE DATATYPES
+let name="maryam"//in string prefer to use ""
+let age=21//in number->2 to power 53 
+let isloggedin=true//boolean
+//null-> standalone value(represent empty)
+//undefined->value not assigned
+//symbol-> to show or find uniqueness
+
+//object
+console.log(typeof null);//datatype-> object
+console.log(typeof undefined);
+//datatype-> undefined bcz a type
