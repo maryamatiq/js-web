8ae1815669d439301302ac33bb64fed20984e93b
Maryam Atiq <maryamatiq768@gmail.com>
2026-07-01 19:52:11 +0000

    dates and time

---
+++ basics/datetime.js
@@ -0,0 +1,28 @@
+//date
+let anydate=new Date()
+//console.log(anydate);
+//console.log(anydate.toString())
+// console.log(anydate.toDateString());
+// console.log(anydate.toLocaleString())
+// console.log(anydate.toISOString());
+// console.log(anydate.toJSON());
+//console.log(typeof anydate)
+
+let createdate=new Date(2026,6,2)
+//let createdate=new Date(2026,6,2,12,20)
+//let createdate=new Date("2026-7-2")
+//console.log(createdate.toLocaleString());
+
+//Time
+let Timespan=Date.now();
+// console.log(Timespan);
+// console.log(createdate.getTime());
+//console.log(Math.floor(Date.now()/1000));//to convert into sec
+
+let letdate=new Date()
+console.log(letdate);
+
+console.log(letdate.getMonth()+1);
+console.log(letdate.getDay().toString());
+console.log(letdate.getFullYear());
+
