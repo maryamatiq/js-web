9d215f7f3210122255c7dfea8eb2bc4823f766aa
Maryam Atiq <maryamatiq768@gmail.com>
2026-07-07 21:42:27 +0000

    if else

---
+++ ControlFlow/first.js
@@ -0,0 +1,61 @@
+//if
+// if(true){
+//     console.log("hello");
+// }
+
+const isLoggedin=true
+// if(isLoggedin){
+//     console.log("execute");
+// }
+
+//COMPARISON
+// <,>,==,!=,<=,>=,===
+
+//  if(2!=8){
+//     console.log("execute");
+// }
+
+//if-else
+const tempreture=41
+// if(tempreture<50){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("greater than 50");
+// }
+
+// if else if
+// const marks=80
+// if(marks<=60){
+//     console.log("Grade C");
+// }
+// else if(marks<=70){
+//     console.log("Grade B");
+// }
+// else if(marks<=80){
+//     console.log("Grade A");
+// }
+
+// if(2==2) console.log("equals to two");
+//IT IS POSSIBLE BUT NOT A GOOD PRACTICE
+
+
+// const coldweather="no"
+// if(tempreture==41){
+//     const hotweather="yes"
+//     console.log(`${coldweather} it is not a cold weather today`);
+// }
+/*console.log(`${hotweather} it is a hot weather today`);*/
+
+const isloggedin=true
+const paymentdone=true
+if(isloggedin&&paymentdone&&true){
+    console.log("you'll easily access the website");   
+}
+
+const debitcard=true
+const byCash=false
+if(debitcard||byCash||false){
+    console.log("you'll easily access the website");  
+}
+
