b5092811c91f235d4046a1e64b3595172ab5bdd0
Maryam Atiq <maryamatiq768@gmail.com>
2026-07-11 20:03:19 +0000

    while or do while

---
+++ Iterations/two.js
@@ -0,0 +1,27 @@
+
+//let i=0;
+// while (i<=10) {
+//     //console.log(i);
+//    // i=i+2
+// }
+
+
+// let arr=["png","jpg","pdf","doc"]
+// let j=0;
+// while (j<arr.length) {
+//    console.log(arr[j]);
+//     j++
+// }
+
+
+// let num=1
+// do {
+//    console.log(num);
+//     num=num+2
+// } while (num<20);
+
+let num=11
+do {
+  console.log(num);
+    num++
+} while (num<=10);
